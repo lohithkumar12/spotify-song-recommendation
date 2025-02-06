cba05176478c9823d37dd72ba6f4e36d68719a64
lohithkumar12 <vemuboddupalli@gmail.com>
2025-02-06 21:45:05 +0530

    Changed common.py file

--- spotify_recommendation/utils/common.py
+++ spotify_recommendation/utils/common.py
@@ -4,6 +4,7 @@ import json
 import joblib
 from ensure import ensure_annotations
 #from box import ConfigBox
+from box import Box
 from box.box import Box
 from pathlib import Path
 from typing import Any
